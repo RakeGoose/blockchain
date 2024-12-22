6652ab3800069aa077b05dfec0782d25520c0b83
rustem <230324@astanait.edu.kz>
2024-12-22 16:44:15 +0500

    Creating validation of the current block hash and previuous hash

--- assignment_1/blockchain.js
+++ assignment_1/blockchain.js
@@ -42,4 +42,21 @@ class Blockchain {
         console.log('Block successfully added to the chain!');
     }
 
+    isValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
+module.exports = Blockchain;
