0c7f2e76404b706046c068416f06631473707daa
pop <230184@astanait.edu.kz>
2024-12-22 15:29:26 +0500

    privet

--- assignment_1/merkleTree.js
+++ assignment_1/merkleTree.js
@@ -1,14 +1,19 @@
-class Transaction {
-    constructor(sender, receiver, amount) {
-        this.sender = sender;
-        this.receiver = receiver;
-        this.amount = amount;
-    }
+const SHA256 = require('./hash');
 
-    // Returns a string representation of the transaction
-    toString() {
-        return `${this.sender}->${this.receiver}:${this.amount}`;
+// Merkle Tree Implementation
+function buildMerkleTree(transactions) {
+    let merkleLeaves = transactions.map(tx => SHA256(tx.toString()));
+
+    while (merkleLeaves.length > 1) {
+        const newLevel = [];
+        for (let i = 0; i < merkleLeaves.length; i += 2) {
+            const left = merkleLeaves[i];
+            const right = merkleLeaves[i + 1] || left; // Handle odd nodes
+            newLevel.push(SHA256(left + right));
+        }
+        merkleLeaves = newLevel;
     }
+    return merkleLeaves[0] || '0'; // Root of Merkle Tree (or '0' if no transactions)
 }
 
-module.exports = Transaction;
+module.exports = buildMerkleTree;
