e9cfd62e598f12aa32a277a0a715c09ab2f67be8
pop <230184@astanait.edu.kz>
2025-01-08 21:19:03 +0500

    hlafp

--- assignment_1/index.js
+++ assignment_1/index.js
@@ -2,17 +2,26 @@ const Blockchain = require('./blockchain');
 const Transaction = require('./transaction');
 const RSA = require('./rsa');
 
-const blockchain = new Blockchain();
+const { privateKey: privateKeyRustem, publicKey: publicKeyRustem } = RSA.generateKeyPair();
+const { privateKey: privateKeyDaniyal, publicKey: publicKeyDaniyal } = RSA.generateKeyPair();
 
-for(let i = 1; i <= 15; i++){
-    blockchain.addTransaction(
-        new Transaction(`Rustem ${i}`, `Daniyal ${i}`, Math.floor(Math.random()*100))
-    );
+const blockchain = new Blockchain(2);
+
+for (let i = 1; i <= 15; i++) {
+    const sender = `Rustem${i}`;
+    const receiver = `Daniyal${i}`;
+    const amount = Math.floor(Math.random() * 100);
+
+    const privateKey = privateKeyRustem;
+    const publicKey = publicKeyRustem;
+
+    const transaction = new Transaction(sender, receiver, amount);
+
+    blockchain.addTransaction(transaction, privateKey, publicKey);
 }
 
 blockchain.mineBlock();
 blockchain.mineBlock();
 
-console.log("Is blockchain valid? ", blockchain.isValid());
-
+console.log('Is blockchain valid?', blockchain.isValid());
 console.log(JSON.stringify(blockchain, null, 2));
